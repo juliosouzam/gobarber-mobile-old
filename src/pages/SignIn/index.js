b13e9a057764f1f745f75905fb87868d40a12cc3
Julio Cesar <julio.souzam@gmail.com>
2020-03-10 22:39:24 -0300

    feat: structure, background, inputs, buttons and accessibility

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,53 @@
+import React, { useRef } from 'react';
+import { Image } from 'react-native';
+
+import Background from '~/components/Background';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
+import logo from '~/assets/logo.png';
+
+export default function SignIn({ navigation }) {
+  const passwordRef = useRef();
+
+  async function handleSubmit() {}
+
+  return (
+    <Background>
+      <Container>
+        <Image source={logo} />
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCaptalize="none"
+            placeholder="Digite seu e-mail"
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Digite sua senha"
+            ref={passwordRef}
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
+          />
+
+          <SubmitButton onPress={handleSubmit}>Acessar</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => navigation.navigate('SignUp')}>
+          <SignLinkText>Criar conta gratuita</SignLinkText>
+        </SignLink>
+      </Container>
+    </Background>
+  );
+}
